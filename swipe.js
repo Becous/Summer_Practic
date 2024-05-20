18acd143abd130243169a49dec3bd4fbb94cdbc8
Becous <becousgame@gmail.com>
2024-05-20 17:57:51 +0300

    правки

--- swipe.js
+++ swipe.js
@@ -1,11 +1,16 @@
 import Swiper from "swiper";
-// import Swiper styles
+import { Pagination } from "swiper/modules";
 import "swiper/css";
+Swiper.use([Pagination]);
 
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
+  autoplay: {
+    delay: 3000,
+  },
   loop: false,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 });
